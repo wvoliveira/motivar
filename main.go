27fbd6b4d62e7aaf41e39a8910a9abe7e89f08b3
wvoliveira <oliveira@live.it>
2020-09-23 01:46:54 -0300

    change to package and add simple test to test language

--- main.go
+++ main.go
@@ -1,186 +1,57 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"math/rand"
+	"motivar/motivar"
 	"os"
 	"path"
 	"time"
 
-	"github.com/markbates/pkger"
 	"github.com/mitchellh/go-homedir"
-
-	"gopkg.in/ini.v1"
 )
 
-var (
-	name    string = "motivar"
-	version string = "v0.1"
-
-	homeDir string
-
-	cfgDir     string
-	cfgFile    string
-	cfgDataDir string
-
-	flagLanguage string
-
-	banner string = fmt.Sprintf(`
-              ._ o o
-              \_´-)|_
-           ,""       \
-         ,"  ## |   ಠ ಠ. 
-       ," ##   ,-\__    ´.
-     ,"       /     ´--._;)
-   ,"     ## / Motivar %v
- ,"   ##    /
-
- `, version)
-)
-
-type phrase struct {
-	Quote  string `json:"quote"`
-	Author string `json:"author"`
-}
+var cfg motivar.Conf
 
 func init() {
 	homeDir, err := homedir.Dir()
-	check(err)
+	motivar.Check(err)
 
-	cfgDir = path.Join(homeDir, ".motivar")
-	cfgFile = path.Join(cfgDir, "motivar.ini")
-	cfgDataDir = path.Join(cfgDir, "data")
+	cfg.Dir = path.Join(homeDir, ".motivar")
+	cfg.File = path.Join(cfg.Dir, "motivar.ini")
+	cfg.DataDir = path.Join(cfg.Dir, "data")
 
-	err = setup()
-	check(err)
+	err = cfg.Setup()
+	motivar.Check(err)
 
-	flag.StringVar(&flagLanguage, "language", "br", "Choose a language to show quotes [br,us]")
-	flag.StringVar(&flagLanguage, "l", "br", "Choose a language to show quotes [br,us]")
+	flag.StringVar(&cfg.Language, "language", "br", "Choose a language to show quotes [br,us]")
+	flag.StringVar(&cfg.Language, "l", "br", "Choose a language to show quotes [br,us]")
 
 	flag.Usage = func() {
 		var CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-		fmt.Fprintf(CommandLine.Output(), banner)
-		fmt.Fprintf(CommandLine.Output(), "Usage of %s:\n", name)
+		fmt.Fprintf(CommandLine.Output(), motivar.Banner)
+		fmt.Fprintf(CommandLine.Output(), "Usage of %s:\n", motivar.Name)
 		flag.PrintDefaults()
 	}
 	flag.Parse()
 
-	readEnv()
+	cfg.ReadEnv()
 
-	err = checkLanguages()
-	check(err)
+	err = motivar.CheckLanguages(cfg.Language)
+	motivar.Check(err)
 }
 
 func main() {
-	pathLang := fmt.Sprintf("/data/%v/", flagLanguage)
+	pathLang := fmt.Sprintf("/data/%v/", cfg.Language)
 
-	var quotes []phrase
-	err := readPhrases(pathLang, &quotes)
-	check(err)
+	var quotes []motivar.Phrase
+	err := motivar.ReadPhrases(pathLang, &quotes)
+	motivar.Check(err)
 
 	rand.Seed(time.Now().UnixNano())
 	v := rand.Intn(len(quotes)-1) + 1
 
 	q := quotes[v]
 	fmt.Printf("%+v %+v\n", q.Quote, q.Author)
-}
-
-func check(e error) {
-	if e != nil {
-		fmt.Printf("error: %+v\n", e)
-		os.Exit(1)
-	}
-}
-
-func checkLanguages() error {
-	if !(flagLanguage == "br" || flagLanguage == "us") {
-		return errors.New("Language not supported. Use br or us")
-	}
-	return nil
-}
-
-func setup() error {
-	// check and create conf dir
-	if _, err := os.Stat(cfgDir); os.IsNotExist(err) {
-		err := os.Mkdir(cfgDir, 0764)
-		if err != nil {
-			return err
-		}
-	}
-
-	// check and create conf file
-	if _, err := os.Stat(cfgFile); os.IsNotExist(err) {
-		_, err := os.Create(cfgFile)
-		if err != nil {
-			return err
-		}
-
-		err = makeCfg()
-		if err != nil {
-			return err
-		}
-	}
-
-	// create data dir
-	if _, err := os.Stat(cfgDataDir); os.IsNotExist(err) {
-		err := os.Mkdir(cfgDataDir, 0764)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func makeCfg() error {
-	cfg, err := ini.Load(cfgFile)
-	if err != nil {
-		return err
-	}
-
-	cfg.Section("").Key("language").SetValue("br")
-	err = cfg.SaveTo(cfgFile)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func readPhrases(path string, p *[]phrase) error {
-	pkger.Walk(path, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if !info.IsDir() {
-			f, err := pkger.Open(path)
-			defer f.Close()
-
-			content, err := ioutil.ReadAll(f)
-			if err != nil {
-				return err
-			}
-
-			err = json.Unmarshal(content, &p)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	})
-
-	return nil
-}
-
-func readEnv() {
-	language := os.Getenv("MOTIVAR_LANGUAGE")
-	if language != "" {
-		if language == "br" || language == "us" {
-			flagLanguage = language
-		}
-	}
 }
---
+++ motivar/motivar_test.go
@@ -0,0 +1,29 @@
+package motivar
+
+import (
+	"testing"
+)
+
+func TestCheckLanguages(t *testing.T) {
+	langError := "Language not supported. Use 'br' or 'us'"
+	languages := []struct {
+		lang   string
+		result string
+	}{
+		{"br", ""},
+		{"us", ""},
+		{"fr", langError},
+		{"ko", langError},
+		{"la", langError},
+		{"zh", langError},
+	}
+
+	for _, l := range languages {
+		err := CheckLanguages(l.lang)
+		if err != nil {
+			if err.Error() != l.result {
+				t.Errorf("Language %v was not supported, got: %v, want: %v.", l.lang, err, l.result)
+			}
+		}
+	}
+}
